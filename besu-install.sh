c9222f74468431bc9c6b2e9859f150ef752387a8
sam bacha <sam@freighttrust.com>
2020-05-26 14:54:53 -0700

    :
    removing a colon

--- besu-install.sh
+++ besu-install.sh
@@ -25,7 +25,7 @@ ntpq -p
 sudo apt update -y
 sudo apt-get update -y
 
-wget https://dl.bintray.com/hyperledger-org/besu-repo/:besu-1.4.5.tar.gz
+wget https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.4.5.tar.gz
 tar xzvf besu-1.4.5.tar.gz
 cd besu-1.4.5
 mkdir node
